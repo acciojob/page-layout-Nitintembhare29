2636bb956f4558d394c8f2c60bdcd4d23b9ebbb3
Nitin Tembhare <nitintembhare340@gmail.com>
2023-08-07 18:58:23 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <header>Welcome to my website</header>
+        <PageLayout>
+          <P>This is the content of my website.</P>
+        </PageLayout>
+        <footer><span>&#169;</span> 2023 My Website. All rights reserved.</footer>
     </div>
   )
 }
---
+++ src/components/PageLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PageLayout({children}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default PageLayout
